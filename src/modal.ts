fd31ddf486a4252a712fb1baedce2e706479afe0
Sultan Badri <46205282+SultanBadri@users.noreply.github.com>
2020-11-29 20:42:44 -0800

    modal bug fix

--- src/modal.ts
+++ src/modal.ts
@@ -1,3 +1,4 @@
+const modalContainer = <HTMLDivElement>document.querySelector("#error-modal-container");
 const modal = <HTMLDivElement>document.querySelector(".error-modal");
 const xIcon = <HTMLDivElement>document.querySelector(".close-modal");
 const closeButton = <HTMLButtonElement>document.querySelector("button");
@@ -7,9 +8,11 @@ let modalOpen = false;
 function toggleErrorModal(): void {
   if (modalOpen) {
     modal.style.transform = "scale(0)";
+    modalContainer.style.pointerEvents =  "none";
     overlay.style.opacity = "0";
   } else {
     modal.style.transform = "scale(1)";
+    modalContainer.style.pointerEvents =  "auto";
     overlay.style.opacity = "1";
   }
 }
